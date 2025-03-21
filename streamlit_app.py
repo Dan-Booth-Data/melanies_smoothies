dd8c553a31215c3777df90a9b9b37dbad8111135
Dan-Booth-Data <DanielBooth347@Gmail.com>
2025-03-21 17:52:28 +0000

    Update streamlit_app.py
    smoothiefroot

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,3 +58,9 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
+
+# New section for smoothiefruit
+
+import requests
+smoothiefroot_response = requests.get("https://smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
