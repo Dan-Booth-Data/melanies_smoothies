22f81352fad8d642c8c63f09ec2073aacdb71f93
Dan-Booth-Data <DanielBooth347@Gmail.com>
2025-03-21 18:28:12 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 # Pandas DF
 pd_df = my_dataframe.to_pandas()
 st.dataframe(pd_df)
-st.stop()
+#st.stop()
 
 
 ingredients_list = st.multiselect(
