9448c36258dc7bd936a61f7a46f12885596fe9ee
Dan-Booth-Data <DanielBooth347@Gmail.com>
2025-03-21 17:55:13 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -62,5 +62,5 @@ if ingredients_list:
 # New section for smoothiefruit
 
 import requests
-smoothiefroot_response = requests.get("https://smoothiefroot.com/api/fruit/watermelon")
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 st.text(smoothiefroot_response)
